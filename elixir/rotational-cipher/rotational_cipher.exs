74b93f2dce7caf7144448361131d0b8dad6c6415
Nikhil <nikhilpant1997@gmail.com>
2018-06-27 16:08:14 +0800

    rotational-cipher complete

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -8,5 +8,20 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    text
+    |> to_charlist()
+    |> Enum.map(fn(x) -> shift_char(x, shift) end)
+    |> to_string()
   end
+
+  def shift_char(code_char, shift) when code_char in ?a..?z,
+    do: shift_with_base(code_char, shift, ?a)
+
+  def shift_char(code_char, shift) when code_char in ?A..?Z,
+    do: shift_with_base(code_char, shift, ?A)
+
+  def shift_char(code_char, _), do: code_char
+
+  def shift_with_base(code_char, shift, base),
+   do: base + rem(code_char + shift - base, 26)
 end
