93caf2680a98ea324c7b77ca1dc2c6c19f6e89e2
Nikhil <nikhilpant1997@gmail.com>
2018-06-28 09:23:59 +0800

    Finished protein-translation, added pig-latin

--- elixir/protein-translation/protein_translation.exs
+++ elixir/protein-translation/protein_translation.exs
@@ -4,8 +4,28 @@ defmodule ProteinTranslation do
   """
   @spec of_rna(String.t()) :: {atom, list(String.t())}
   def of_rna(rna) do
+    rna
+    |> String.split("", trim: true)
+    |> rna_helper([])
   end
 
+  def rna_helper([], pre),  do: {:ok, pre}
+
+  def rna_helper([first | []],  pre), do: {:error, "invalid RNA"}
+
+  def rna_helper([first |[second | []]], pre), do: {:error, "invalid RNA"}
+
+  def rna_helper([first | [second | [third | tail]]], pre) do
+      {_status, protein} = of_codon(first <> second <> third)
+      case (protein) do
+        "invalid codon" ->
+          {:error, "invalid RNA"}
+        "STOP" ->
+          {:ok , pre}
+        _ ->
+          rna_helper(tail, (pre ++ [protein]))
+      end
+  end
   @doc """
   Given a codon, return the corresponding protein
 
@@ -29,5 +49,25 @@ defmodule ProteinTranslation do
   """
   @spec of_codon(String.t()) :: {atom, String.t()}
   def of_codon(codon) do
+    case(codon) do
+      "UGU" -> {:ok, "Cysteine"}
+      "UGC" -> {:ok, "Cysteine"}
+      "UUA" -> {:ok, "Leucine"}
+      "UUG" -> {:ok, "Leucine"}
+      "AUG" -> {:ok, "Methionine"}
+      "UUU" -> {:ok, "Phenylalanine"}
+      "UUC" -> {:ok, "Phenylalanine"}
+      "UCU" ->{:ok, "Serine"}
+      "UCC" -> {:ok, "Serine"}
+      "UCA" -> {:ok, "Serine"}
+      "UCG" -> {:ok, "Serine"}
+      "UGG" -> {:ok, "Tryptophan"}
+      "UAU" -> {:ok, "Tyrosine"}
+      "UAC" -> {:ok, "Tyrosine"}
+      "UAA" -> {:ok, "STOP"}
+      "UAG" -> {:ok, "STOP"}
+      "UGA" -> {:ok, "STOP"}
+      _else -> {:error, "invalid codon"}
+    end
   end
 end
